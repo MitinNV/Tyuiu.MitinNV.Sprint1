f4b7e9ce660c79401a616c557f77d232e901131a
MitinNV <Tyuiu.MitinNV@outlook.com>
2024-09-25 21:51:28 +0500

    task1

---
+++ Tyuiu.MitinNV.Sprint1.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MitinNV.Sprint1.Task1.V27.Lib;
+
+namespace Tyuiu.MitinNV.Sprint1.Task1.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.5, res);
+        }
+    }
+}
---
+++ Tyuiu.MitinNV.Sprint1.Task1.V27/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.MitinNV.Sprint1.Task1.V27.Lib;
+namespace Tyuiu.MitinNV.Sprint1.Task1.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Митин Никита Владимирович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Организация ввода | вывода в консольных приложениях                       *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнил: Митин Никита Владимирович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу, которая запрашивает у пользователя исходные данные, в*");
+            Console.WriteLine("*вычисляет результат по формуле (x+y)/2 и печатает его на экране.         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            double x, y;
+
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            var result = ds.Calculate(x, y);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
