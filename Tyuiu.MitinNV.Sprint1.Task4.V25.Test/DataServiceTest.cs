f4f6bc42d40c0520f3994cffd06750459338f799
MitinNV <tyuiu.mitinnv@outlook.com>
2024-10-02 20:14:26 +0500

    Task4

---
+++ Tyuiu.MitinNV.Sprint1.Task4.V25.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.MitinNV.Sprint1.Task4.V25.Lib;
+
+namespace Tyuiu.MitinNV.Sprint1.Task4.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(0.649, ds.Calculate(1));
+        }
+    }
+}
---
+++ Tyuiu.MitinNV.Sprint1.Task4.V25/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.MitinNV.Sprint1.Task4.V25.Lib;
+namespace Tyuiu.MitinNV.Sprint1.Task4.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил: Митин Никита Владимирович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Class Math                                                              *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Митин Никита Владимирович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу, которая запрашивает у пользователя исходные данные, в*");
+            Console.WriteLine("*вычисляет результат по формуле и печатает его на экране. Ответ округлите *");
+            Console.WriteLine("* до 3 знаков после запятой.                                              *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            Console.WriteLine("Введите a:");
+            var a = Convert.ToDouble(Console.ReadLine());
+
+            var result = ds.Calculate(a);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
